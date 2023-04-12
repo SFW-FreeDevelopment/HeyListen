c92e999666b8468d34ef33aa4666c84bb0afc01c
Christopher Plummer <chrisplummer0194@gmail.com>
2023-04-11 23:43:18 -0400

    Utils for snakecase

---
+++ HeyListen.ETL/SnakeCaseNamingPolicy.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace HeyListen.ETL;
+
+public class SnakeCaseNamingPolicy : JsonNamingPolicy
+{
+    public static SnakeCaseNamingPolicy Instance { get; } = new SnakeCaseNamingPolicy();
+
+    public override string ConvertName(string name)
+    {
+        return name.ToSnakeCase();
+    }
+}
